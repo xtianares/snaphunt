ea40f030cbc97de0aeb4216e504e334b864c2da2
J.Strub <jstrub90@gmail.com>
2019-05-07 20:11:27 -0400

    set up snaps to render on the snaps page

--- client/src/pages/Snaps.js
+++ client/src/pages/Snaps.js
@@ -5,22 +5,42 @@ import API from "../utils/API";
 
 class Snap extends Component {
   state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
+    imageUrls:[]
   };
 
   componentDidMount() {
     console.log("it mounted");
+    API.getSnaps(this.props.match.params.id)
+        .then(res =>  {
+          let snaps = res.data;
+          let imageUrls = [];
+          console.log(res.data);
+          for(let i=0; i < snaps.length; i++) {
+            imageUrls.push(snaps[i].url)
+          }
+          this.setState({ imageUrls: imageUrls });
+        })
+        .then(console.log("state source" + this.state.snapUrl))
+        .catch(err => console.log(err));
+
+    console.log("state source" + this.state.snapUrl);
   }
 
   render() {
+    const images = this.state.imageUrls.map((item, key) =>
+        <img src={item} key={item}/>
+    )
     return (
+      
       <Container>
         <Row className="justify-content-md-center">
           <Col md="8" sm="12">
             <h1 className="text-center">Snaps, nothing here yet</h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md="8" sm="12">
+            {images}
           </Col>
         </Row>
       </Container>
