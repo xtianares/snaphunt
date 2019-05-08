e167d9217dcb595e7fa6c18148e1337e71a9dcb2
xtianares <xtianares@yahoo.com>
2019-05-07 23:28:34 -0400

    working map markers

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import API from "../../utils/API";
 
 class Map extends Component {
   state = {
-    currentLocation: ""
+    currentLocation: "",
+    hunts: []
   };
 
   componentDidMount = () => {
@@ -21,26 +23,50 @@ class Map extends Component {
 
   showPosition = (position) => {
     let currentLocation = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+      lng: position.coords.longitude,
+      lat: position.coords.latitude
     }
     this.setState({ currentLocation: currentLocation });
     // console.log(this.state.imageLocation)
     // return currentLocation;
+    this.getHunts();
+  }
+
+  getHunts = () => {
+    // console.log(this.state.currentLocation.lng, this.state.currentLocation.lat)
+    API.getHuntsNearMe({
+      lng: this.state.currentLocation.lng,
+      lat: this.state.currentLocation.lat
+      // lng: -81.3076798,
+      // lat: 28.744246
+    })
+      .then(huntData => {
+        // console.log(userData.data);
+        if(huntData.data != null && huntData.data.errmsg == null){
+          console.log(huntData.data);
+          this.setState({ hunts: huntData.data });
+        }
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
     const googleMapKey = process.env.REACT_APP_GOOGLEMAP_API_KEY;
-    const googleMapURL = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapKey + '&v=3.exp&libraries=geometry,drawing'
+    const googleMapURL = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapKey + '&v=3.exp&libraries=geometry,drawing';
+    const huntMarkers = this.state.hunts.map((item, key) =>
+      <Marker key={item._id} position={item.location} />
+    );
     const SnapHuntMap = withScriptjs(withGoogleMap(props => (
       <GoogleMap
         defaultCenter = { this.state.currentLocation }
-        defaultZoom = { 17.5 }
+        defaultZoom = { 17 }
       >
         <Marker position={this.state.currentLocation} />
-        <Marker position={{lat: 28.744246, lng: -81.3076798}} text="The School" />
+        {/*<Marker position={{lng: -81.3076798, lat: 28.744246}} text="The School" />*/}
+        {huntMarkers}
       </GoogleMap>
     )));
+    console.log(huntMarkers);
 
     return (
       <SnapHuntMap
