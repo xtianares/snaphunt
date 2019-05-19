a798fd8d15f47def483085c4eb0fa1ac0c0c471e
xtianares <xtianares@yahoo.com>
2019-05-19 16:03:12 -0400

    fixed hunts keywords creation

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -35,25 +35,32 @@ class Create extends Component {
 
   handleInputChange = event => {
     const { name, value } = event.target;
+    // this.setState({
+    //   [name]: (value).toLowerCase()
+    // });
     if (name.indexOf("keyword") >= 0) {
       let theKeys = this.state.keywords;
       // console.log("currentKeys:");
       // console.log(theKeys);
       // console.log("inputValue: " + value);
       // console.log(theKeys.indexOf(value));
-      if (value !== '' && value !== null && theKeys.indexOf(value) < 0) {
-        theKeys.push(value);
+      if (theKeys.indexOf(value) < 0) {
+        // console.log(name);
+        let inputKey = parseFloat(name.replace("keyword", ""));
+        // console.log(inputKey);
+        theKeys[inputKey -1] = value;
       }
       this.setState({
         keywords: theKeys
       });
+      // console.log(this.state.keywords)
     }
     else {
       this.setState({
         [name]: (value).toLowerCase()
       });
     }
-    // console.log(this.state.keywords)
+    console.log(this.state.keywords)
   };
 
   handleFormSubmit = event => {
@@ -64,27 +71,27 @@ class Create extends Component {
           huntName: this.state.huntName,
           location: this.state.location,
           user: this.state.userId,
-          keywords: this.state.keywords
+          keywords: this.state.keywords.filter(Boolean)
       })
-        .then(huntData => {
-          // console.log(huntData.data);
-          if(huntData.data != null && huntData.data.errmsg == null){
-            this.setState({
-              huntName: "",
-              location: "",
-              userId: "",
-              keywords: []
-            });
-            event.target.reset();
-            console.log(huntData.data);
-          }
-          // if user does not exist
-          else {
-            let err = "Something went wrong!";
-            console.log(err);
-          }
-        })
-        .catch(err => console.log(err));
+      .then(huntData => {
+        // console.log(huntData.data);
+        if(huntData.data != null && huntData.data.errmsg == null){
+          this.setState({
+            huntName: "",
+            location: "",
+            userId: "",
+            keywords: []
+          });
+          event.target.reset();
+          console.log(huntData.data);
+        }
+        // if user does not exist
+        else {
+          let err = "Something went wrong!";
+          console.log(err);
+        }
+      })
+      .catch(err => console.log(err));
     }
   };
 
@@ -101,19 +108,19 @@ class Create extends Component {
               </FormGroup>
               <FormGroup>
                 <Label>Keywords (minimum of 3)</Label>
-                <Input onBlur={this.handleInputChange} type="text" name="keyword1" placeholder="Keyword 1" />
+                <Input onChange={this.handleInputChange} type="text" name="keyword1" placeholder="Keyword 1" />
               </FormGroup>
               <FormGroup>
-                <Input onBlur={this.handleInputChange} type="text" name="keyword2" placeholder="Keyword 2" />
+                <Input onChange={this.handleInputChange} type="text" name="keyword2" placeholder="Keyword 2" />
               </FormGroup>
               <FormGroup>
-                <Input onBlur={this.handleInputChange} type="text" name="keyword3" placeholder="Keyword 3" />
+                <Input onChange={this.handleInputChange} type="text" name="keyword3" placeholder="Keyword 3" />
               </FormGroup>
               <FormGroup>
-                <Input onBlur={this.handleInputChange} type="text" name="keyword4" placeholder="Keyword 4" />
+                <Input onChange={this.handleInputChange} type="text" name="keyword4" placeholder="Keyword 4" />
               </FormGroup>
               <FormGroup>
-                <Input onBlur={this.handleInputChange} type="text" name="keyword5" placeholder="Keyword 5" />
+                <Input onChange={this.handleInputChange} type="text" name="keyword5" placeholder="Keyword 5" />
               </FormGroup>
               <FormGroup className="text-center">
                 <Button type="submit">Create Hunt</Button>
